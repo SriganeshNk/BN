06d0dd7518e3f2933a0a4207531e702152dbc933
Chidambaram Ramanathan <chidambaram.ramanath@gmail.com>
2014-11-07 19:42:03 -0500

    Update question2_solver.py
    new changes

--- question2_solver.py
+++ question2_solver.py
@@ -16,36 +16,6 @@ class Question2_Solver:
     #    query: "que__ion";
     #    return ["s", "t"];
     def solve(self, query):
-	alph='abcdefghijklmnopqrstuvwxyz'
-	missing_index = query.index('_')
-	
-	# Get all possible combinations
-	set_words = [query.replace('_',c,1) for c in alph]
-	possible_words = [ '`'+st.replace('_',c,1)+'`' for st in set_words for c in alph ]
-
-#	print possible_words
-	
-	pr_list = []
-	for st in possible_words:
-		pr = 1
-		len_st = len(st)-1
-		i = 0
-		for c in st:
-			if i < len_st:
-					pr = pr*self.cpt.conditional_prob(st[i+1], c)
-					if(pr == 0):
-						break
-					i = i + 1
-		pr_list.append(pr)
-
-	# Get the maximum prbability 
-	pr = max(pr_list)
-
-	# Find the word with maximum probability
-	ind = pr_list.index(pr)
-	final_string = possible_words[ind]
- #     	print "Final string = ",final_string 
-	 
 	return [final_string[missing_index+1], final_string[missing_index+2]];
 
 
